6e2d6600b411f7c3896a54b398d8047b1b4e1054
Harry Xiong <harryxiong24@gmail.com>
2024-06-19 16:10:50 -0700

    2024.6.19

--- ObjectiveC Study/ObjectiveC Study/Class/Class.h
+++ ObjectiveC Study/ObjectiveC Study/Class/Class.h
@@ -1,12 +1,12 @@
 //
-//  OCClass.h
+//  Class.h
 //  ObjectiveC Study
 //
 //  Created by Harry Xiong on 2024/6/18.
 //
 
-#ifndef OCClass_h
-#define OCClass_h
+#ifndef Class_h
+#define Class_h
 
 
 // 类的声明: 声明对象的属性，行为
@@ -16,16 +16,19 @@
     // 用来声明对象属性（实例变量＼成员变量 ，默认会初始化为0）
     // @public 可以让外部的指针间接访问对象内部的成员变量
     @public
-    int wheels; // 轮胎个数
-    int speed; // 时速(xxkm/h)
+        int wheels; // 轮胎个数
+        int speed; // 时速(xxkm/h)
 }
 
 // 方法（行为）：方法名，参数，返回值（声明，实现）
 // 要是 OC 对象的方法，必须以减号 －
 //  OC 方法中的小括号（）：括住数据类型, OC 方法中任何数据类型都必须用小括号（）括住
-- (void) run;
+- (void)run;
+- (void)fly;
 
 @end
 
+void testClass(void);
 
-#endif /* OCClass_h */
+
+#endif /* Class_h */
--- ObjectiveC Study/ObjectiveC Study/main.m
+++ ObjectiveC Study/ObjectiveC Study/main.m
@@ -6,38 +6,18 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "OCClass/OCClass.h"
-
-void demoOCClass(void);
-
-
-void demoOCClass(void)
-{
-    // 在oc中，想执行一些行为，就写上一个中括号[行为执行者 行为名称]
-    
-    // 定义了一个指针变量 Car1,Car1 将来指向的是 Car 类型的对象
-    // [Car new] 会创建一个新对象，并且会返回对象本身（新对象地址 ）
-    Car *car1 = [Car new];
-    Car *car2 = [Car new];
-    
-    car2->wheels = 5;
-    car2->speed = 300;
-    
-    // 给Car1所指向对象的wheels属性赋值
-    car1->wheels = 4;
-    car1->speed = 250;
-    
-    // 给Car1所指向对象发送一条run消息
-    [car1 run];
-    
-    NSLog(@"车子有%d个轮子，时速位：%dkm/h", car1->wheels, car2->speed);
-}
-
+#import "Class/Class.h"
+#import "Class/Method.h"
+#import "Class/Encapsulate.h"
+#import "Class/ClassMethod.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
 
-        demoOCClass();
+        testClass();
+        testMethod();
+        testEncapsulate();
+        testClassMethod();
         
         return 0;
     }
